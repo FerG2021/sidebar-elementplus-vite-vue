8cba9997cb20bb6cd5348359cf3fd001b60418fc
Fernando Gonzalez <fernandojaviergonzalez2018@gmail.com>
2022-06-05 00:07:53 -0300

    primer comentario

---
+++ src/views/about/About.vue
@@ -0,0 +1,16 @@
+<template>
+    <main class="about-page">
+        <h1>About</h1>
+        <p>Esta es la pagina de about</p>
+    </main>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+
+</style>
